dfa0737e9c56d79da626a41a8bc73b3f7c5ea717
ImmortalDovah07 <kyleclawson3@gmail.com>
2025-10-21 13:18:27 -0400

    Made the triangle in starting position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
@@ -8,14 +9,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
 
+        time = clock.tick(60)
+        dt = time / 1000
         pygame.display.flip()
 
 
