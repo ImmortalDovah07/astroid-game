d7d40fff5efcc44fb265c21fbeb41915b4b046e7
ImmortalDovah07 <kyleclawson3@gmail.com>
2025-10-21 13:39:05 -0400

    Added movement to the player object

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -19,7 +24,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
-        player.draw(screen)
+
+        for thing in drawable:
+            thing.draw(screen)
+        
+        updatable.update(dt)
 
         time = clock.tick(60)
         dt = time / 1000
