087d0d2047bb878980744be062963277775d941d
ImmortalDovah07 <kyleclawson3@gmail.com>
2025-10-27 14:46:42 -0400

    Working on Collision

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
-
-    clock = pygame.time.Clock()
-    dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black")
-
-        for thing in drawable:
-            thing.draw(screen)
         
         updatable.update(dt)
 
-        time = clock.tick(60)
-        dt = time / 1000
+        screen.fill("black")
+
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 
 
